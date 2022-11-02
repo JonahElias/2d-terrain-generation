e4c1a03d6fd93230065bc46fa4b6f7b9a328946a
Jonah Elias <jonahelias26@gmail.com>
2022-11-02 09:51:37 -0700

    different noise layers

--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,13 @@ public class Main {
 
     public static void main(String[] args){
         Random r = new Random();
-        int length = 100;
+        int length = 500;
         int [][] map = TileGenerator.generateArray(length, r.nextInt());
         StdDraw.setCanvasSize(650, 650);
         StdDraw.setScale(0, length);
-
-        for(int y = 0; y < length; y++) {
-            for (int x = 0; x < length; x++) {
+        StdDraw.picture(length / 2, length / 2, "map.png", length, length);
 
 
-                int num = map[x][y];
-                if (num == 0){
-                    StdDraw.setPenColor(Color.BLUE);
-                }else{
-                    StdDraw.setPenColor(Color.GREEN);
-                }
-                StdDraw.filledSquare(x + 0.5, y + 0.5, 0.5);
-                //StdDraw.filledCircle(x, y + 1, 0.5);
-
-
-
-            }
-
-        }
-        StdDraw.save("map.png");
     }
 
 
--- src/TileGenerator.java
+++ src/TileGenerator.java
@@ -1,6 +1,8 @@
+import noise.ImageExporter;
 import noise.Noise;
 
 import javax.annotation.processing.SupportedSourceVersion;
+import java.awt.image.BufferedImage;
 import java.util.Arrays;
 
 public class TileGenerator {
@@ -10,6 +12,43 @@ public class TileGenerator {
         return (b - a) * (x - min) / (max - min) + a;
     }
 
+    public static int terrainOne(Noise noise, int x, int y){
+        float noiseValue = noise.interpolatedNoise(x * .02f, y * .02f);
+        noiseValue += noise.interpolatedNoise(x * 0.08f, y * 0.08f);
+        noiseValue /= 2;
+        float scaled = scaleRange(-1, 1, 0, 1, noiseValue);
+        int height = Math.round(scaled);
+        return height;
+    }
+
+    public static int terrainTwo(Noise noise, int x, int y){
+        float noiseValue = 0;
+        noiseValue += scaleRange(-1, 1, 0, 1, noise.interpolatedNoise(x * 0.01f, y * 0.01f));
+        noiseValue += scaleRange(-1, 1, 0, 1, noise.interpolatedNoise(x * 0.02f, y * 0.02f));
+        noiseValue += scaleRange(-1, 1, 0, 1, noise.interpolatedNoise(x * 0.04f, y * 0.04f));
+        noiseValue += scaleRange(-1, 1, 0, 1, noise.interpolatedNoise(x * 0.08f, y * 0.08f));
+        int roundedValue = Math.round(noiseValue / 4f);
+        return roundedValue;
+    }
+
+    public static int getRGBValue(Noise noise, int height){
+        //float noiseValue = noise.smoothNoise(x, y);
+        int scaled = Math.round(scaleRange(-1, 1, 0, 255, height));
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+
+       if(scaled > 155) {
+            red = 140;
+            green = 166;
+            blue = 83;
+        }else {
+            red = 30;
+            green = 110;
+            blue = 160;
+        }
+        return ((255 << 24) | red << 16 | green << 8 | blue);
+    }
 
 
 
@@ -20,38 +59,17 @@ public class TileGenerator {
             for (int x = 0; x < map[0].length; x++) {
 
 
-                float noiseValue = noise.smoothNoise(x, y);
-                float scaled = scaleRange(-1, 1, 0, 1, noiseValue);
-                int height = Math.round(scaled);
-                map[y][x] = height;
-
-
-
-
-
-
-                /*
-                float noiseValue = 0;
-                noiseValue += scaleRange(-1, 1, 0, 1, noise.interpolatedNoise(x * 0.01f, y * 0.01f));
-                noiseValue += scaleRange(-1, 1, 0, 1, noise.interpolatedNoise(x * 0.02f, y * 0.02f));
-
-                noiseValue += scaleRange(-1, 1, 0, 1, noise.interpolatedNoise(x * 0.04f, y * 0.04f));
-                noiseValue += scaleRange(-1, 1, 0, 1, noise.interpolatedNoise(x * 0.08f, y * 0.08f));
-                int roundedValue = Math.round(noiseValue / 4f);
-                map[y][x] = roundedValue;
-
-                 */
-
-
-
-
-
+                int height = terrainOne(noise, x, y);
+                map[y][x] = getRGBValue(noise, height);
                 System.out.println("x: " + x);
                 System.out.println("y : " + y);
                 System.out.println("height : " + map[x][y]);
 
             }
         }
+
+        BufferedImage colorImage = ImageExporter.convertRGBArray(map);
+        ImageExporter.saveAsFile("map.png", colorImage);
 
         return map;
     }
